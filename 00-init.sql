37a374f1f0853eac2ee746d4e546e4cf3eac668e
Malte Kyhos <malte.kyhos@civey.com>
2022-03-05 00:59:44 +0100

    First sketch of new schema

---
+++ 00-init.sql
@@ -0,0 +1,51 @@
+/*
+  Create initial schema
+*/
+
+-- Table "station"
+
+CREATE TABLE station AS (
+  id text PRIMARY KEY
+);
+
+COMMENT ON TABLE station AS 'Table for station-level information';
+
+-- Table "coordinator"
+CREATE TABLE coordinator AS (
+  name TEXT,
+  station REFERENCES station(id),
+  contact TEXT,
+  ts_start TIMESTAMP WITHOUT TIMEZONE,
+  ts_end TIMESTAMP WITHOUT TIMEZONE,
+  role TEXT,
+  tasks TEXT,
+  comment TEXT,
+  PRIMARY KEY(name, station, contact, ts_start)
+);
+COMMENT ON TABLE coordinator AS 'Table on coordinators per station';
+
+-- Table "volunteer"
+CREATE TABLE volunteer AS (
+  name TEXT,
+  station REFERENCES station(id),
+  contact TEXT,
+  ts_start TIMESTAMP WITHOUT TIMEZONE,
+  ts_end TIMESTAMP WITHOUT TIMEZONE,
+  share_contact BOOLEAN DEFAULT FALSE,
+  languages JSONB,
+  PRIMARY KEY (name, station, contact, ts_start);
+);
+COMMENT ON TABLE volunteer AS 'Table of registered volunteers';
+
+
+-- TABLE "incoming"
+CREATE TABLE incoming AS (
+  station REFERENCES station(id),
+  id TEXT,
+  ts_scheduled TIMESTAMP WITHOUT TIMEZONE,
+  ts_delay TIMESTAMP WITHOUT TIMEZONE DEFAULT NULL,
+  type TEXT,
+  passengers INT,
+  PRIMARY KEY (station, id, ts_scheduled)
+);
+COMMENT ON TABLE incoming AS 'Table on incoming trains and buses';
